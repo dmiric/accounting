ed0b2656200eb15911f0bff94170759df1b6131f
dmiric <damir.miric@gmail.com>
2024-01-06 11:36:43 +0100

    .

--- backend/Code.js
+++ backend/Code.js
@@ -7,7 +7,7 @@ function aCreateMenu_() {
   menu.addToUi()
 }
 
-// test 2236d5
+// test 2236d57
 function doGet() {
   const htmlServ = HtmlService.createTemplateFromFile("find-a-document");
   return htmlServ.evaluate()
